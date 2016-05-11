6c4b751e11f7df22e915327150d8f444e80f2d93
root <root@piha.cse.ucsc.edu>
2016-05-11 14:43:27 -0700

    Add Dockerfile for ga4gh-server

---
+++ ga4gh/ga4gh-server/config.py
@@ -0,0 +1,10 @@
+import os
+
+#TODO this logic could move to frontend.configure() or BaseConfig
+# For docker, if/when a default is set in serverconfig.py, run -v /localdata:/default-path
+# If the env variable GA4GH_DATA_SOURCE is set, use that path. Otherwise, use the default path
+DATA_SOURCE = os.getenv('GA4GH_DATA_SOURCE', "/srv/ga4gh/ga4gh-example-data")
+
+# If the env variable GA4GH_DEBUG is set, use that. Otherwise, use the empty string (False)
+# Enable with True
+DEBUG = os.getenv('GA4GH_DEBUG', "")
