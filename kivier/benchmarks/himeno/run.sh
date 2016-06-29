337cf692ab9543fdc992668277dbeeea591259bf
Jianshen Liu <jliu120@ucsc.edu>
2016-06-28 23:12:31 -0700

    Add run script for Himeno benchmark

---
+++ kivier/benchmarks/himeno/run.sh
@@ -0,0 +1,34 @@
+#!/bin/bash -e
+
+if [ "$#" -ne 2 ]; then
+    cat <<-ENDOFMESSAGE
+Please specify the Grid-size and the output file as arguments.
+Usage: ./run.sh <XS/S/M/L/XL> <output file>
+ENDOFMESSAGE
+    exit
+fi
+
+FOLDER_NAME=himeno
+if [ ! -f $FOLDER_NAME/himenoBMTxpa ]; then
+    while true; do
+        read -p "Do you wish to install Himeno benchmark? [y/n] " yn
+        case $yn in
+            [Yy]* )
+                mkdir $FOLDER_NAME
+                wget -O $FOLDER_NAME/himenoBMTxpa.c https://github.com/ljishen/srl/raw/master/kivier/benchmarks/himeno/docker/himenoBMTxpa.c
+                gcc $FOLDER_NAME/himenoBMTxpa.c -O3 -o $FOLDER_NAME/himenoBMTxpa
+                rm $FOLDER_NAME/himenoBMTxpa.c
+                break
+                ;;
+            [Nn]* )
+                exit
+                ;;
+            *     )
+                echo "Please answer yes or no."
+                ;;
+        esac
+    done
+fi
+
+mkdir $(dirname $1)
+$FOLDER_NAME/himenoBMTxpa $1 | tee $2
